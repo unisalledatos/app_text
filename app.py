6744efecfc27c6fa53b3a98ad11234411e9c20d3
unisalledatos <123432461+unisalledatos@users.noreply.github.com>
2023-10-31 11:01:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pickle
 import numpy as np
 import sklearn
-import nltk.tokenizer
+import nltk
+nltk.downlowad('punkt')
 
 doc = open('model.pickle', 'rb')
 model = pickle.load(doc)
